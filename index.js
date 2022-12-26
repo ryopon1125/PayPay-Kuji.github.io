0d14e5a1f88ab245c44d5ed700db499daf4f1e0c
ryopon1125 <109675183+ryopon1125@users.noreply.github.com>
2022-12-26 10:49:17 +0900

    Add files via upload
    Signed-off-by: ryopon1125 <109675183+ryopon1125@users.noreply.github.com>

---
+++ index.js
@@ -0,0 +1,6 @@
+<script type="text/javascript">
+   function gate() {
+      var UserInput = prompt("パスワードを入力して下さい:"PayPay-Kuji-2022-Ryopon,"");
+      location.href = UserInput + ".html";
+   }
+</script>
